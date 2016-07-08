09a95a7f7177995fbcd5d2573c44d4f8254c3b09
jonpage3 <jonpage3@gmail.com>
2016-07-08 12:36:51 -0400

    adding all the files from assignment 3

--- jonpage3-convert-docs.sh
+++ jonpage3-convert-docs.sh
@@ -1,13 +1,16 @@
-
+#!/bin/bash
 
 # Declare input argument as a variable
-
+var=$1
 # Convert markdown to HTML
-
+pandoc -o $var.html $var.md
 # Convert markdown to DOCX
-
+pandoc -o $var.docx $var.md
 # Convert markdown to ODT
-
+pandoc -o $var.odt $var.md
 # Convert markdown to PDF
-
+pandoc -o $var.pdf $var.md
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted $var to HTML, DOCX, ODT, PDF"
+
+
